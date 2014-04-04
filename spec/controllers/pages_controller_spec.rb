e0a8479f5751c096ecb41817108984e5ac60d411
Ganesh Ganesan <g.alaguganesh@gmail.com>
2014-04-04 16:57:29 -0500

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,6 +14,12 @@ describe PagesController do
 			#response.should have_selector("title",  :content => "Ruby on Rails Tutorial Sample App | Home")\
 			response.body.should include 'Ruby on Rails Tutorial Sample App | Home'
 		end
+		
+		it "should have a non-blank body" do
+			get :home
+			response.body.should_not =~ /<body>\s*<\/body>/
+		end
+		
 	end
 	
 	describe "GET 'contact' " do
@@ -21,12 +27,23 @@ describe PagesController do
 			get :contact
 			response.should be_success
 		end
+		
+		it "should have the right title" do
+			get :contact
+			response.body.should include 'Ruby on Rails Tutorial Sample App | Contact'
+		end
+		
 	end
 	
 	describe "GET 'about' " do
 		it	"should be successful" do
 			get :about
 			response.should be_success
+		end
+		
+		it "should have the right title" do
+			get :about
+			response.body.should include 'Ruby on Rails Tutorial Sample App | About'
 		end
 	end
 
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,9 +6,15 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
   
-  test "should get the right title" do
+  test "should get the right title for home page" do
 	get :home
 	assert_select 'title', "Ruby on Rails Tutorial Sample App | Home"
+  end
+  
+  test "body should not be blank" do
+	get :home
+	#regex need to be fixed
+	assert_select 'body', /\s*/
   end
   
   test "should get contact" do
@@ -16,9 +22,19 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
   
+    test "should get the right title for contact page" do
+	get :contact
+	assert_select 'title', "Ruby on Rails Tutorial Sample App | Contact"
+  end
+  
   test "should get about" do
 	get :about
 	assert_response :success
 	end
+	
+	test "should get the right title for about page" do
+	get :about
+	assert_select 'title', "Ruby on Rails Tutorial Sample App | About"
+  end
 	
 end
